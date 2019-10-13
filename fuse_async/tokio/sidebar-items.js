8cdaff83f50bd5651e75ae50fcf2756f99da0a28
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-10-13 17:35:26 +0000

    Automated deployment: Sun Oct 13 17:35:26 UTC 2019 614eaa1efafe9a3a74eb1b53cdc7b06f8be100b4

--- fuse_async/tokio/sidebar-items.js
+++ fuse_async/tokio/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["default_shutdown_signal",""],["mount","Run a FUSE filesystem mounted on the specified path."]],"struct":[["Channel","Asynchronous I/O object that communicates with the FUSE kernel driver."]]});
+initSidebarItems({"fn":[["default_shutdown_signal","Create a signal future that captures some kind of signals."],["mount","Run a FUSE filesystem mounted on the specified path."]],"struct":[["Channel","Asynchronous I/O object that communicates with the FUSE kernel driver."]]});
