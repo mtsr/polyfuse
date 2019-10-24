0abad1e24622c69581bb56b08738ebf4b558696e
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-10-24 12:58:59 +0000

    Automated deployment: Thu Oct 24 12:58:59 UTC 2019 6d9f0ae367c0c121f05c8a00afa1fa020ece99d5

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["polyfuse"] = {"name":"","files":["buf.rs","conn.rs","dir.rs","lib.rs","main_loop.rs","op.rs","reply.rs","session.rs","tokio.rs"]};
+sourcesIndex["polyfuse"] = {"name":"","files":["buf.rs","conn.rs","dir.rs","fs.rs","lib.rs","main_loop.rs","reply.rs","session.rs","tokio.rs"]};
 sourcesIndex["polyfuse_abi"] = {"name":"","files":["lib.rs","parse.rs"]};
 sourcesIndex["polyfuse_sys"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();
