20f15335f67d73b9a9f4b164b7b3adef4c1a07cd
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-10-24 16:25:43 +0000

    Automated deployment: Thu Oct 24 16:25:43 UTC 2019 490e44f6319df8824a166f01f95fb4b9c7742054

--- implementors/futures_io/if_std/trait.AsyncWrite.js
+++
@@ -1,9 +0,0 @@
-(function() {var implementors = {};
-implementors["polyfuse"] = [{text:"impl <a class=\"trait\" href=\"https://rust-lang-nursery.github.io/futures-api-docs/0.3.0-alpha.19/futures_io/futures_io/if_std/trait.AsyncWrite.html\" title=\"trait futures_io::if_std::AsyncWrite\">AsyncWrite</a> for <a class=\"struct\" href=\"polyfuse/tokio/struct.Channel.html\" title=\"struct polyfuse::tokio::Channel\">Channel</a>",synthetic:false,types:["polyfuse::tokio::Channel"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
--- polyfuse/tokio/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["default_shutdown_signal","Create a signal future that captures some kind of signals."],["mount","Run a FUSE filesystem mounted on the specified path."]],"struct":[["Channel","Asynchronous I/O object that communicates with the FUSE kernel driver."]]});
