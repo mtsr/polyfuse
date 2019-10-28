7978236322d9d315b0f6806cbd539a201df68281
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-10-28 05:36:15 +0000

    Automated deployment: Mon Oct 28 05:36:15 UTC 2019 29718a21f916f0cb29a212c6651a8e2827181ae8

---
+++ implementors/core/marker/trait.StructuralPartialEq.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["polyfuse_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"polyfuse_sys/abi/enum.fuse_opcode.html\" title=\"enum polyfuse_sys::abi::fuse_opcode\">fuse_opcode</a>",synthetic:false,types:["polyfuse_sys::abi::fuse_opcode"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"polyfuse_sys/abi/enum.fuse_notify_code.html\" title=\"enum polyfuse_sys::abi::fuse_notify_code\">fuse_notify_code</a>",synthetic:false,types:["polyfuse_sys::abi::fuse_notify_code"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
