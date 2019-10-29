9f7cb2d00c27db109f508360bfd4d7a2c2766f8a
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-10-29 06:17:32 +0000

    Automated deployment: Tue Oct 29 06:17:32 UTC 2019 d009a51120d4271b1504f447144eba0bf2a68b94

--- polyfuse/fs/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["Operation","The kind of FUSE requests received from the kernel."]],"struct":[["Context","Contextural information about an incoming request."],["FileAttr",""],["FileLock",""],["FsStatistics",""]],"trait":[["Filesystem","The filesystem running on the user space."]]});
