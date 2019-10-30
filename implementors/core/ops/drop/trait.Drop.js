76e3287d812d6cefdc79cf35e6e66753f43ce23f
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-10-30 12:05:07 +0000

    Automated deployment: Wed Oct 30 12:05:07 UTC 2019 b8aee74c75afd8e00cb09a6d1039957d940415a6

--- implementors/core/ops/drop/trait.Drop.js
+++
@@ -1,9 +0,0 @@
-(function() {var implementors = {};
-implementors["polyfuse"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"polyfuse/server/struct.Channel.html\" title=\"struct polyfuse::server::Channel\">Channel</a>",synthetic:false,types:["polyfuse::server::Channel"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
