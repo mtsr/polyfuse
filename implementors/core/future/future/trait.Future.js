34ef268e81476a0c1fd51c3a0370fa4c18cb65f3
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-11-01 11:34:28 +0000

    Automated deployment: Fri Nov  1 11:34:28 UTC 2019 47e449d42c11e8157fa102dc5833c889bbad9226

---
+++ implementors/core/future/future/trait.Future.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["polyfuse"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"polyfuse/session/struct.Interrupt.html\" title=\"struct polyfuse::session::Interrupt\">Interrupt</a>",synthetic:false,types:["polyfuse::session::Interrupt"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ implementors/futures_core/future/trait.FusedFuture.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["polyfuse"] = [{text:"impl <a class=\"trait\" href=\"https://rust-lang-nursery.github.io/futures-api-docs/0.3.0-alpha.19/futures_core/futures_core/future/trait.FusedFuture.html\" title=\"trait futures_core::future::FusedFuture\">FusedFuture</a> for <a class=\"struct\" href=\"polyfuse/session/struct.Interrupt.html\" title=\"struct polyfuse::session::Interrupt\">Interrupt</a>",synthetic:false,types:["polyfuse::session::Interrupt"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
