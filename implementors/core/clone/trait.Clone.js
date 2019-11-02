5e4f2f8bd5ef9108c4235a015dc23942bf58cb39
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-11-02 16:25:19 +0000

    Automated deployment: Sat Nov  2 16:25:18 UTC 2019 83e6890a44eae210688b4ac08ec24edefdb80614

--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["polyfuse"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"polyfuse/struct.Notifier.html\" title=\"struct polyfuse::Notifier\">Notifier</a>",synthetic:false,types:["polyfuse::server::Notifier"]},];
 implementors["polyfuse_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"polyfuse_sys/abi/enum.fuse_opcode.html\" title=\"enum polyfuse_sys::abi::fuse_opcode\">fuse_opcode</a>",synthetic:false,types:["polyfuse_sys::abi::fuse_opcode"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"polyfuse_sys/abi/enum.fuse_notify_code.html\" title=\"enum polyfuse_sys::abi::fuse_notify_code\">fuse_notify_code</a>",synthetic:false,types:["polyfuse_sys::abi::fuse_notify_code"]},];
 
             if (window.register_implementors) {
--- implementors/core/future/future/trait.Future.js
+++ implementors/core/future/future/trait.Future.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["polyfuse"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"polyfuse/session/struct.Interrupt.html\" title=\"struct polyfuse::session::Interrupt\">Interrupt</a>",synthetic:false,types:["polyfuse::session::Interrupt"]},];
+implementors["polyfuse"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"polyfuse/session/struct.Interrupt.html\" title=\"struct polyfuse::session::Interrupt\">Interrupt</a>",synthetic:false,types:["polyfuse::session::Interrupt"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"polyfuse/session/struct.NotifyRetrieve.html\" title=\"struct polyfuse::session::NotifyRetrieve\">NotifyRetrieve</a>",synthetic:false,types:["polyfuse::session::NotifyRetrieve"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
