a97bfcde7468b4ddcbb7f9d1024b61992f84377b
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-11-03 09:40:44 +0000

    Automated deployment: Sun Nov  3 09:40:44 UTC 2019 366c79dd797670a5e9a56792487361b4737f61b6

---
+++ implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["polyfuse"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"polyfuse/session/struct.DirEntryType.html\" title=\"struct polyfuse::session::DirEntryType\">DirEntryType</a>",synthetic:false,types:["polyfuse::session::dirent::DirEntryType"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ implementors/core/marker/trait.StructuralEq.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["polyfuse"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"polyfuse/session/struct.DirEntryType.html\" title=\"struct polyfuse::session::DirEntryType\">DirEntryType</a>",synthetic:false,types:["polyfuse::session::dirent::DirEntryType"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
