a256ec98b6519e9fdcb678214cf2371e424091c0
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-11-10 09:37:49 +0000

    Automated deployment: Sun Nov 10 09:37:48 UTC 2019 3a71590004c77975c7962a105d2b6266dfe111cd

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["polyfuse"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"polyfuse/struct.Lock.html\" title=\"struct polyfuse::Lock\">Lock</a>&lt;T&gt;",synthetic:false,types:["polyfuse::lock::Lock"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"polyfuse/channel/struct.Connection.html\" title=\"struct polyfuse::channel::Connection\">Connection</a>",synthetic:false,types:["polyfuse::channel::conn::Connection"]},];
+implementors["polyfuse"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"polyfuse/channel/struct.Connection.html\" title=\"struct polyfuse::channel::Connection\">Connection</a>",synthetic:false,types:["polyfuse::channel::conn::Connection"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
