73970734cba0cab6d9d35fb2b8f008adc53b0816
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-11-11 07:47:33 +0000

    Automated deployment: Mon Nov 11 07:47:33 UTC 2019 9d7a517c90243f8e94f2fe0ed7c67a21967ac28b

---
+++ implementors/core/ops/bit/trait.Not.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["polyfuse"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"struct\" href=\"polyfuse/session/struct.CapabilityFlags.html\" title=\"struct polyfuse::session::CapabilityFlags\">CapabilityFlags</a>",synthetic:false,types:["polyfuse::session::CapabilityFlags"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
