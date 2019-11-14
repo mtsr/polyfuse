28de072e317f23a7c50a8a6b63c177865345ba59
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-11-14 15:48:46 +0000

    Automated deployment: Thu Nov 14 15:48:46 UTC 2019 f9feeb7bfe0e3b080fda1786915270dfd6b6e536

--- implementors/futures_io/if_std/trait.AsyncWrite.js
+++
@@ -1,9 +0,0 @@
-(function() {var implementors = {};
-implementors["polyfuse_tokio"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/futures-io/0.3.0/futures_io/if_std/trait.AsyncWrite.html\" title=\"trait futures_io::if_std::AsyncWrite\">AsyncWrite</a> for <a class=\"struct\" href=\"polyfuse_tokio/struct.Channel.html\" title=\"struct polyfuse_tokio::Channel\">Channel</a>",synthetic:false,types:["polyfuse_tokio::channel::Channel"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
---
+++ implementors/polyfuse/reply/trait.ReplyWriter.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["polyfuse"] = [];
+implementors["polyfuse_tokio"] = [{text:"impl <a class=\"trait\" href=\"polyfuse/reply/trait.ReplyWriter.html\" title=\"trait polyfuse::reply::ReplyWriter\">ReplyWriter</a> for <a class=\"struct\" href=\"polyfuse_tokio/struct.Channel.html\" title=\"struct polyfuse_tokio::Channel\">Channel</a>",synthetic:false,types:["polyfuse_tokio::channel::Channel"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
