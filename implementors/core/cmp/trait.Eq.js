68accffa2c369470cb505c5be40157ed18e546e5
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-11-15 12:05:29 +0000

    Automated deployment: Fri Nov 15 12:05:29 UTC 2019 96911e94f7c824dcf50c94149ee8823f31e166ef

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["polyfuse"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"polyfuse/struct.DirEntryType.html\" title=\"struct polyfuse::DirEntryType\">DirEntryType</a>",synthetic:false,types:["polyfuse::dirent::DirEntryType"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"polyfuse/struct.CapabilityFlags.html\" title=\"struct polyfuse::CapabilityFlags\">CapabilityFlags</a>",synthetic:false,types:["polyfuse::init::CapabilityFlags"]},];
+implementors["polyfuse"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"polyfuse/struct.CapabilityFlags.html\" title=\"struct polyfuse::CapabilityFlags\">CapabilityFlags</a>",synthetic:false,types:["polyfuse::init::CapabilityFlags"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
