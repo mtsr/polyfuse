ca401df45d3c878859fe587ca367fe640ca97e7d
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-11-28 06:41:37 +0000

    Automated deployment: Thu Nov 28 06:41:37 UTC 2019 7b48d7bc408d753033a4b21df927b52fd8420a7c

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["polyfuse"] = {"name":"","files":["common.rs","dirent.rs","fs.rs","init.rs","lib.rs","notify.rs","reply.rs","request.rs","session.rs"]};
+sourcesIndex["polyfuse"] = {"name":"","files":["common.rs","dirent.rs","fs.rs","init.rs","lib.rs","notify.rs","op.rs","reply.rs","request.rs","session.rs"]};
 sourcesIndex["polyfuse_examples"] = {"name":"","dirs":[{"name":"memfs","files":["inode.rs","mod.rs","table.rs"]}],"files":["lib.rs"]};
 sourcesIndex["polyfuse_sys"] = {"name":"","files":["kernel.rs","lib.rs"]};
 sourcesIndex["polyfuse_tokio"] = {"name":"","files":["channel.rs","lib.rs","mount.rs","server.rs"]};
