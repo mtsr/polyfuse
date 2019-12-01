f55a4e63fb0123355112783735c622e4a57fd0f7
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-12-01 16:07:24 +0000

    Automated deployment: Sun Dec  1 16:07:24 UTC 2019 33a6256b79db0ca6eb7dc32a8786f84addc8a7b6

--- implementors/core/future/future/trait.Future.js
+++ implementors/core/future/future/trait.Future.js
@@ -1,6 +1,5 @@
 (function() {var implementors = {};
 implementors["polyfuse"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"polyfuse/notify/struct.RetrieveHandle.html\" title=\"struct polyfuse::notify::RetrieveHandle\">RetrieveHandle</a>&lt;T&gt;",synthetic:false,types:["polyfuse::notify::RetrieveHandle"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"polyfuse/struct.Interrupt.html\" title=\"struct polyfuse::Interrupt\">Interrupt</a>",synthetic:false,types:["polyfuse::session::Interrupt"]},];
-implementors["polyfuse_tokio"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"polyfuse_tokio/struct.RetrieveHandle.html\" title=\"struct polyfuse_tokio::RetrieveHandle\">RetrieveHandle</a>",synthetic:false,types:["polyfuse_tokio::server::RetrieveHandle"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/futures_core/future/trait.FusedFuture.js
+++ implementors/futures_core/future/trait.FusedFuture.js
@@ -1,6 +1,5 @@
 (function() {var implementors = {};
 implementors["polyfuse"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://docs.rs/futures-core/0.3.0/futures_core/future/trait.FusedFuture.html\" title=\"trait futures_core::future::FusedFuture\">FusedFuture</a> for <a class=\"struct\" href=\"polyfuse/notify/struct.RetrieveHandle.html\" title=\"struct polyfuse::notify::RetrieveHandle\">RetrieveHandle</a>&lt;T&gt;",synthetic:false,types:["polyfuse::notify::RetrieveHandle"]},{text:"impl <a class=\"trait\" href=\"https://docs.rs/futures-core/0.3.0/futures_core/future/trait.FusedFuture.html\" title=\"trait futures_core::future::FusedFuture\">FusedFuture</a> for <a class=\"struct\" href=\"polyfuse/struct.Interrupt.html\" title=\"struct polyfuse::Interrupt\">Interrupt</a>",synthetic:false,types:["polyfuse::session::Interrupt"]},];
-implementors["polyfuse_tokio"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/futures-core/0.3.0/futures_core/future/trait.FusedFuture.html\" title=\"trait futures_core::future::FusedFuture\">FusedFuture</a> for <a class=\"struct\" href=\"polyfuse_tokio/struct.RetrieveHandle.html\" title=\"struct polyfuse_tokio::RetrieveHandle\">RetrieveHandle</a>",synthetic:false,types:["polyfuse_tokio::server::RetrieveHandle"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
